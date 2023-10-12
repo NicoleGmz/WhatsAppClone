84f852c7f143401db55b26913e0e62763ef0adf4
Nicole G <nicolegp94@gmail.com>
2023-10-12 12:01:13 +0100

    Modification in the navigation file

--- app/src/main/java/com/nicole/whatsappclone/WhatsappDestinations.kt
+++ app/src/main/java/com/nicole/whatsappclone/WhatsappDestinations.kt
@@ -4,21 +4,21 @@ interface WhatsappDestination{
     val route: String
 }
 object ChatsList: WhatsappDestination{
-    override val route = "chats_list"
+    override val route = "Chats"
 }
 
 
 object NotWorking: WhatsappDestination{
-    override val route = "not_working"
+    override val route = "Not Working"
 }
 
 
 object Calls: WhatsappDestination{
-    override val route = "calls"
+    override val route = "Calls"
 }
 
 object States: WhatsappDestination{
-    override val route = "states"
+    override val route = "States"
 }
 
 val whatsappTabRowScreens = listOf(ChatsList, Calls, States)
